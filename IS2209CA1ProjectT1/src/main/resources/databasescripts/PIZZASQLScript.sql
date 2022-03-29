fd12a402c0eb6f9a89847aa8b2d9a5d9d9776f38
CALUMONEILL <120475986@umail.ucc.ie>
2022-03-29 19:38:22 +0100

    Updated PIZZASQLScript

--- IS2209CA1ProjectT1/src/main/resources/databasescripts/PIZZASQLScript.sql
+++ IS2209CA1ProjectT1/src/main/resources/databasescripts/PIZZASQLScript.sql
@@ -16,3 +16,29 @@ insert into pizza (name, price) values('Hawaiian', 11.99);
 insert into pizza (name, price) values('Margherita', 9.99);
 insert into pizza (name, price) values('Vegetarian', 11.99);
 insert into pizza (name, price) values('Seafood', 12.99);
+
+--Create Ingredient Table with autoid, name, calories, allergens
+CREATE TABLE INGREDIENT (ID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY, "NAME" VARCHAR(100), "CALORIES" INT, "ALLERGENS" VARCHAR(100), PRIMARY KEY (ID));
+
+--Insert ten rows of data into ingredient table
+insert into ingredient (name, calories, allergens) values('Cheese', 90, 'Dairy');
+insert into ingredient (name, calories, allergens) values('Tomato Sauce', 30, '');
+insert into ingredient (name, calories, allergens) values('Pepperoni', 100, 'Dairy');
+insert into ingredient (name, calories, allergens) values('Ham', 90, '');
+insert into ingredient (name, calories, allergens) values('Pineapple', 50, '');
+insert into ingredient (name, calories, allergens) values('Shrimp', 80, 'Shellfish');
+insert into ingredient (name, calories, allergens) values('Crab Meat', 90, 'Shellfish');
+insert into ingredient (name, calories, allergens) values('Peppers', 40, '');
+insert into ingredient (name, calories, allergens) values('Sweetcorn', 40, '');
+insert into ingredient (name, calories, allergens) values('Olives', 30, '');
+
+--Create PIZZAINGREDIENTLOOKUP with autoid, foreign keys pizzaid and ingredientid
+CREATE TABLE PIZZAINGREDIENTLOOKUP (
+ID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
+"PIZZAID" INT,
+    FOREIGN KEY ("PIZZAID") REFERENCES PIZZA (ID),
+"INGREDIENTID" INT,
+    FOREIGN KEY ("INGREDIENTID") REFERENCES INGREDIENT (ID),
+PRIMARY KEY (ID)
+);
+
