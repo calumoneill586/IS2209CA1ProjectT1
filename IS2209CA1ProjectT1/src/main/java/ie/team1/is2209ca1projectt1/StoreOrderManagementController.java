874f9ca3b4dde342b564d055d13c3cc0597d8b1d
darakelly122 <120392506@umail.ucc.ie>
2022-03-30 13:38:04 +0100

    Store management files

---
+++ IS2209CA1ProjectT1/src/main/java/ie/team1/is2209ca1projectt1/StoreOrderManagementController.java
@@ -0,0 +1,27 @@
+package ie.team1.is2209ca1projectt1;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Daram
+ */
+public class StoreOrderManagementController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
