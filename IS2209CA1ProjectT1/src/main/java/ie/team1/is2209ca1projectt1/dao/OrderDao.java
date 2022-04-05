6f5771f31de66f5d704d3622a5d2b9a310b41368
orladolan <orladolan14@gmail.com>
2022-04-05 22:30:49 +0100

    Fixing GUI of ConfirmOrder

--- IS2209CA1ProjectT1/src/main/java/ie/team1/is2209ca1projectt1/dao/OrderDao.java
+++ IS2209CA1ProjectT1/src/main/java/ie/team1/is2209ca1projectt1/dao/OrderDao.java
@@ -10,21 +10,17 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.sql.*;
-import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.control.Label;
 
 
 public class OrderDao {
     private Connection conn;
     private String connectionString = "jdbc:derby://localhost:1527/pizzadatabase";
+    private Label lblNumber;
     
     public OrderDao() {
         
@@ -49,12 +45,7 @@ public class OrderDao {
         try {
 
             Statement stmt = conn.createStatement();
-
-
-
             String sql = "SELECT * FROM ORDERS";
-
-
             ResultSet rs = stmt.executeQuery(sql);
 
             while (rs.next()) {
@@ -84,7 +75,13 @@ public class OrderDao {
 
         return orders;
     }
-           
+    
+
+   
+    
+        
+
+
     
         
      //   return orderToAdd;
