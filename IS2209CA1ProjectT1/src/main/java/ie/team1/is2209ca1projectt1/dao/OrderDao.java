da737558c62971df7790f7b374a138367c7eb660
orladolan <orladolan14@gmail.com>
2022-04-05 13:45:02 +0100

    Fixing OrderDao merge conflicts

--- IS2209CA1ProjectT1/src/main/java/ie/team1/is2209ca1projectt1/dao/OrderDao.java
+++ IS2209CA1ProjectT1/src/main/java/ie/team1/is2209ca1projectt1/dao/OrderDao.java
@@ -42,15 +42,10 @@ public class OrderDao {
     
     public ObservableList<Order> getOrders() {
 
-<<<<<<< HEAD
       // List<Order>orders = new ArrayList<Order>();
             ObservableList<Order>orders =  FXCollections.observableArrayList();
           //  orders.add(new Order(1,2, "sds", "uhg", "hghg"));
-            
-=======
-        List<Order> orders = new ArrayList<Order>();
-        
->>>>>>> 9ca3a184b1c894b8f9cb7293c6c2f77b4d1b5766
+
         try {
 
             Statement stmt = conn.createStatement();
@@ -64,15 +59,9 @@ public class OrderDao {
 
             while (rs.next()) {
 
-<<<<<<< HEAD
-                int orderid = rs.getInt("ID");
-=======
-                int id = rs.getInt("ID");
-                String name = rs.getString("TYPE");
-                double price = rs.getInt("PRICE");
-              
 
->>>>>>> 9ca3a184b1c894b8f9cb7293c6c2f77b4d1b5766
+                int orderid = rs.getInt("ID");
+
                 int customerid = rs.getInt("CUSTOMERID");
                 
                 String paymethod = rs.getString("PAYMETHOD");
