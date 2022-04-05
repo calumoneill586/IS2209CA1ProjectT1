c3bc8c1b2cc8ba51ba14dad804fec48d175c2c4f
orladolan <orladolan14@gmail.com>
2022-04-05 14:49:34 +0100

    Making the starter page, staff login and subsequent code behind them

---
+++ IS2209CA1ProjectT1/src/main/java/ie/team1/is2209ca1projectt1/StaffLoginController.java
@@ -0,0 +1,76 @@
+package ie.team1.is2209ca1projectt1;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+
+public class StaffLoginController implements Initializable {
+
+//Login Methods Section
+    @FXML
+    private Button btnStaffLogIn;   
+    @FXML
+    private PasswordField txtStaffPassword;  
+    
+    //Log In Button  
+    @FXML
+    private void handleLogIn() throws IOException { //This section validates password entry
+    Window owner = btnStaffLogIn.getScene().getWindow();
+    
+     
+        System.out.println(txtStaffPassword.getText());
+           
+        if (txtStaffPassword.getText().isEmpty()) {
+          showAlert(Alert.AlertType.ERROR, owner, "Form Error!", 
+              "Please enter the password");
+          return;
+        }
+        
+        String password = txtStaffPassword.getText();
+               
+           if (!"password".equals(password)) {
+            infoBox("Please enter correct Password", null, "Failed");
+        } else {
+            infoBox("Login Successful!", null, "Success");
+            Parent root = FXMLLoader.load(getClass().getResource("ManagementMenu.fxml"));
+    
+            Stage login = (Stage) btnStaffLogIn.getScene().getWindow();
+            login.setScene(new Scene(root, 713,400));
+    }
+            
+        }
+        
+        public static void infoBox(String infoMessage, String headerText, String title) {
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setContentText(infoMessage);
+        alert.setTitle(title);
+        alert.setHeaderText(headerText);
+        alert.showAndWait();
+    } 
+        private static void showAlert(Alert.AlertType alertType, Window owner, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.initOwner(owner);
+        alert.show();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
+} 
+        
--- IS2209CA1ProjectT1/src/main/java/ie/team1/is2209ca1projectt1/StartPageController.java
+++ IS2209CA1ProjectT1/src/main/java/ie/team1/is2209ca1projectt1/StartPageController.java
@@ -1,14 +1,37 @@
 package ie.team1.is2209ca1projectt1;
 
-
-
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 
 public class StartPageController implements Initializable {
+    //Button for moving to Login Page
+    @FXML
+    private Button btnStart, btnStaffLogin;
+ 
+    @FXML
+    private void handleGetStarted() throws IOException {
+         Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
 
+        Stage getStarted = (Stage) btnStart.getScene().getWindow();
+        getStarted.setScene(new Scene(root, 602,299));
+    }
+    
+    @FXML
+    private void handleStaffLogin() throws IOException {
+         Parent root = FXMLLoader.load(getClass().getResource("StaffLogin.fxml"));
+
+        Stage getStarted = (Stage) btnStaffLogin.getScene().getWindow();
+        getStarted.setScene(new Scene(root, 320,299));
+    }
     
     
     @Override
