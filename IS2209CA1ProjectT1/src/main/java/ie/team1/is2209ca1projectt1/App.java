9f7db26b79093e1a6a1d398ed9dd6075a2b7e76f
CALUMONEILL <120475986@umail.ucc.ie>
2022-04-07 17:54:15 +0100

    Functionality added to menu

--- IS2209CA1ProjectT1/src/main/java/ie/team1/is2209ca1projectt1/App.java
+++ IS2209CA1ProjectT1/src/main/java/ie/team1/is2209ca1projectt1/App.java
@@ -16,7 +16,6 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-<<<<<<< HEAD
 
 
 
@@ -27,9 +26,7 @@ public class App extends Application {
 
 
 
-=======
-        scene = new Scene(loadFXML("ConfirmOrder"));
->>>>>>> de8eea6f3acb762d69a2ec6489d5294defb196bb
+
         stage.setScene(scene);
         stage.setTitle("Pizza Me");
         stage.show();
