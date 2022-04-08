b5bc421a1ae8c4752f8431275612aa78d49c627e
orladolan <orladolan14@gmail.com>
2022-04-08 17:02:13 +0100

    Fixed GUI for StorePizzaManagemnt

--- IS2209CA1ProjectT1/src/main/java/ie/team1/is2209ca1projectt1/App.java
+++ IS2209CA1ProjectT1/src/main/java/ie/team1/is2209ca1projectt1/App.java
@@ -15,7 +15,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("CustomerMenu"));
+        scene = new Scene(loadFXML("StartPage"));
         stage.setScene(scene);
         stage.setTitle("Pizza Me");
         stage.show();
--- IS2209CA1ProjectT1/src/main/java/ie/team1/is2209ca1projectt1/StorePizzaManagemntController.java
+++ IS2209CA1ProjectT1/src/main/java/ie/team1/is2209ca1projectt1/StorePizzaManagemntController.java
@@ -1,20 +1,13 @@
 package ie.team1.is2209ca1projectt1;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
-
 import ie.team1.is2209ca1projectt1.dao.Ingredient;
 import ie.team1.is2209ca1projectt1.dao.IngredientDao;
 import ie.team1.is2209ca1projectt1.dao.Pizza;
 import ie.team1.is2209ca1projectt1.dao.PizzaDao;
 import java.io.IOException;
 import java.net.URL;
-import java.sql.PreparedStatement;
 import java.util.List;
 import java.util.ResourceBundle;
-import java.util.jar.Attributes.Name;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -31,24 +24,25 @@ import javafx.stage.Stage;
 public class StorePizzaManagemntController implements Initializable {
     
      @FXML
-    private Button btnAddPizza, btnDeletePizza, btnAddingredient, btnRemoveIngredient, btnBack, btnConfirmSpecialOffers;
+    private Button btnAddPizza, btnDeletePizza, btnAddIngredient, btnRemoveIngredient, btnBack, btnConfirmSpecialOffers;
      
       @FXML
     private ListView lstPizza, lstIngredients, lstSpecialOffers;
       
       @ FXML
-          private Button btnConfirm, btnConfirmIngredient, btnAddIngredient, btnAddOffer;
+          private Button btnConfirm, btnConfirmIngredient, btnAddOffer;
         @ FXML
           private Label lblNewPizza, lblNewIngredient, lblOffer;
          @ FXML
           private TextField txtAddPizza, txtAddIngredient, txtSpecialOffers;
          
          
-         String[] Deals = {"buy 5 get one free", "Buy two get a drink", "buy 10 get free delivery"};
-       PizzaDao dao = new PizzaDao();
-       IngredientDao dao1 = new IngredientDao();
+        String[] Deals = {"buy 5 get one free", "Buy two get a drink", "buy 10 get free delivery"};
+        
+        PizzaDao dao = new PizzaDao();
+        IngredientDao dao1 = new IngredientDao();
      
- @FXML 
+    @FXML 
     public void onBack() throws IOException {
     Parent root = FXMLLoader.load(getClass().getResource("ManagementMenu.fxml"));
     
@@ -59,26 +53,21 @@ public class StorePizzaManagemntController implements Initializable {
        
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-                 txtSpecialOffers.setVisible(false);
-   btnConfirmSpecialOffers.setVisible(false);
-     lblOffer.setVisible(false);
-   
         
-        
+        txtSpecialOffers.setVisible(false);
+        btnConfirmSpecialOffers.setVisible(false);
+        lblOffer.setVisible(false);
+               
         lstSpecialOffers.getItems().addAll(Deals);
         
         
         txtAddPizza.setVisible(false);
         btnConfirm.setVisible(false); 
-          lblNewPizza.setVisible(false); 
-          
-            txtAddIngredient.setVisible(false);
+        lblNewPizza.setVisible(false);
+        txtAddIngredient.setVisible(false);
         btnConfirmIngredient.setVisible(false); 
-          lblNewIngredient.setVisible(false); 
-        
-  
-       
-       
+        lblNewIngredient.setVisible(false); 
+                      
         List<Pizza> pizzas = dao.getPizzas();
         
         lstPizza.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
@@ -89,38 +78,20 @@ public class StorePizzaManagemntController implements Initializable {
             
         }
             
-            List<Ingredient> ingredients = dao1.getIngredients();
+        List<Ingredient> ingredients = dao1.getIngredients();
 
         lstIngredients.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
 
         for (Ingredient ingredient : ingredients) {
             lstIngredients.getItems().add(ingredient);
-        }
-            
-            
-            
-       // List<Ingredient> ingredients = dao1.getIngredients();
-        
-        //lstIngredients.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-        
-        //for(Ingredient ingredient : ingredients) {
-          //  lstIngredients.getItems().add(ingredient);
-        // TODO
-   // }    
-    
+        }         
 }
-    
-    
- //   public HideText (){
-    
-   // }
-    
+      
     
       @FXML
      void onPizzaClick(ActionEvent event) {
  
      int selectedPizza = lstPizza.getSelectionModel().getSelectedIndex();
-  // lstPizza.getItems().remove(selectedPizza);
      if (lstPizza.getSelectionModel().getSelectedIndex()==-1){
          System.out.println("No Pizza selected");
         
@@ -131,109 +102,73 @@ public class StorePizzaManagemntController implements Initializable {
      }
       @FXML  
      void onAddPizzaClick(ActionEvent event) {
-  txtAddPizza.setVisible(true);
-   btnConfirm.setVisible(true);
+     txtAddPizza.setVisible(true);
+     btnConfirm.setVisible(true);
      lblNewPizza.setVisible(true);
      btnAddPizza.setVisible(false);
 
-   //lstPizza.getItems().add(txtAddPizza.getText());
      }
-         @FXML  
+      
+     @FXML  
      void onConfirmClick(ActionEvent event) {
-    
-         
-     
-
-         
          lstPizza.getItems().add(txtAddPizza.getText());
-
-   //lstPizza.getItems().add(txtAddPizza.getText());
      }
      
      
       @FXML  
      void onAddIngredientClick(ActionEvent event) {
-  txtAddIngredient.setVisible(true);
-   btnConfirmIngredient.setVisible(true);
-     lblNewIngredient.setVisible(true);
-     btnConfirmIngredient.setVisible(true);
-       btnAddIngredient.setVisible(false);
-       
-     
-
-   //lstPizza.getItems().add(txtAddPizza.getText());
-     }
-              @FXML  
+         txtAddIngredient.setVisible(true);
+         btnConfirmIngredient.setVisible(true);
+         lblNewIngredient.setVisible(true);
+         btnConfirmIngredient.setVisible(true);
+         btnAddIngredient.setVisible(false);
+          }
+             
+     @FXML  
      void onConfirmIngredientClick(ActionEvent event) {
- lstIngredients.getItems().add(txtAddIngredient.getText());
+    lstIngredients.getItems().add(txtAddIngredient.getText());
 
-   //lstPizza.getItems().add(txtAddPizza.getText());
      }
      
        @FXML
      void onDeleteIngredientClick(ActionEvent event) {
  
-    int selectedIngredient = lstIngredients.getSelectionModel().getSelectedIndex();
-  // lstPizza.getItems().remove(selectedPizza);
-     if (lstIngredients.getSelectionModel().getSelectedIndex()==-1){
+        int selectedIngredient = lstIngredients.getSelectionModel().getSelectedIndex();
+         if (lstIngredients.getSelectionModel().getSelectedIndex()==-1){
          System.out.println("No Ingredient selected");
         
-     }else{
-         
-           lstIngredients.getItems().remove(selectedIngredient);
-     }
-   
-     }
+         }else{
+            lstIngredients.getItems().remove(selectedIngredient);
+              }
+         }
 
          @FXML  
-     void onAddOfferClick(ActionEvent event) {
-  txtSpecialOffers.setVisible(true);
-   btnConfirmSpecialOffers.setVisible(true);
-     lblOffer.setVisible(true);
+          void onAddOfferClick(ActionEvent event) {
+         txtSpecialOffers.setVisible(true);
+         btnConfirmSpecialOffers.setVisible(true);
+         lblOffer.setVisible(true);
    
-       btnAddOffer.setVisible(false);
-       
+         btnAddOffer.setVisible(false);
      
-
-   //lstPizza.getItems().add(txtAddPizza.getText());
      }
-        @FXML
-     void onDeleteOfferClick(ActionEvent event) {
+     
+          @FXML
+        void onDeleteOfferClick(ActionEvent event) {
  
-    int selectedOffers = lstSpecialOffers.getSelectionModel().getSelectedIndex();
-  // lstPizza.getItems().remove(selectedPizza);
-     if (lstSpecialOffers.getSelectionModel().getSelectedIndex()==-1){
-         System.out.println("No Offer selected");
-        
-     }else{
-         
-           lstSpecialOffers.getItems().remove(selectedOffers);
-     }
-     }
+        int selectedOffers = lstSpecialOffers.getSelectionModel().getSelectedIndex();
+  
+        if (lstSpecialOffers.getSelectionModel().getSelectedIndex()==-1){
+             System.out.println("No Offer selected");
+        }else{
+            lstSpecialOffers.getItems().remove(selectedOffers);
+              }
+           }
 
      
-                 @FXML  
-     void onConfirmOffersClick(ActionEvent event) {
- lstSpecialOffers.getItems().add(txtSpecialOffers.getText());
-
-   //lstPizza.getItems().add(txtAddPizza.getText());
-     }
-     
-     
- 
-//String sqlc = "DELETE FROM PIZZA WHERE NAME="+ Name;
-       
-    
-
-
-//    PreparedStatement stmt = conn.prepareStatment(sqlc);
-    //lstPizza.getItems().remove(selectedPizza);
-    //java.sql.Statement statement = conn.createStatement();
-            
-            
- 
-    
-   
+        @FXML  
+        void onConfirmOffersClick(ActionEvent event) {
+        lstSpecialOffers.getItems().add(txtSpecialOffers.getText());
+        }
     }
 
 
