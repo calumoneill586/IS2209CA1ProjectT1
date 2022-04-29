60d3ece6699692f7932cf90f3012f23d5d19b865
EDBD2001 <eoghandolan2001@gmail.com>
2022-04-29 22:20:45 +0100

    GUI

--- IS2209CA1ProjectT1/src/main/java/ie/team1/is2209ca1projectt1/App.java
+++ IS2209CA1ProjectT1/src/main/java/ie/team1/is2209ca1projectt1/App.java
@@ -13,7 +13,7 @@ public class App extends Application {
 
     @Override
         public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("BrowseMenu"));
+        scene = new Scene(loadFXML("PizzaManagementWindowmarkIII"));
         stage.setScene(scene);
         stage.setTitle("Pizza Me");
         stage.show();
