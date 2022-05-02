d007f1b3dbb690a3ed72cf40675c6e497a49f0bc
orladolan <orladolan14@gmail.com>
2022-05-02 21:41:59 +0100

    Changing App.java to StartPage

--- IS2209CA1ProjectT1/src/main/java/ie/team1/is2209ca1projectt1/App.java
+++ IS2209CA1ProjectT1/src/main/java/ie/team1/is2209ca1projectt1/App.java
@@ -14,7 +14,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("PizzaManagementWindowmarkIII"));
+        scene = new Scene(loadFXML("StartPage"));
         stage.setScene(scene);
         stage.setTitle("Pizza Me");
         stage.show();
